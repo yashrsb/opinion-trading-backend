b7ef73e0990ae63d7b3e0a06e075bcfe9411d016
YashRaj-Git <yash.bisht@satschel.com>
2025-03-16 01:00:18 +0530

    update auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,16 +1,19 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
-  const token = req.header("Authorization");
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
+    return res.status(403).json({ message: "Invalid token" });
   }
 };
-
-module.exports = authMiddleware;
